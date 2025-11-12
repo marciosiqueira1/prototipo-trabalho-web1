c4c3aec47fc2b6fc41fa68bf2df14ad82ab02aa2
marciosiqueira1 <marcioslribeiro@gmail.com>
2025-11-11 21:50:39 -0300

    Ultimo commit antes de implementar o botao de busca Pais aleatorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { use, useEffect, useState } from 'react';
-import './App.css';
 import Card from './Card';
 import axios from 'axios';
-import endPoints from './endpoints.json';
-
+import './styles/header.css';
+import './styles/cards.css';
 
 function App() {
-
   const apiNome = "https://restcountries.com/v3.1/name";
 
   const [listaPaises, setListaPaises] = useState([]);
   const [valorInput, setValorInput] = useState('');
 
   const buscaNome = async (valorInput) => {
-
     try{
 
       const input = valorInput.trim();
@@ -49,7 +46,7 @@ function App() {
     <>
     <header>
         <h1>
-          <img src="src\images\logoFlagpedia.png" alt="" id='logo'/>
+          <img src="public\images\logoFlagpedia.png" alt="" id='logo'/>
         </h1>
         <nav>
             <div id='div-btn-2'>
@@ -59,31 +56,36 @@ function App() {
               <button id='btn-logout'>LogOut</button>
           </nav>
       </header>
-      <form onSubmit={handleSubmit}>
-        <input type="text" 
-          placeholder='nome do país em inglês'
-          value={valorInput}
-          onChange={(e) => setValorInput(e.target.value)}  
-        />
-        <button type='submit' id='btn-busca'>Buscar</button>
-      </form>
-      <div id='div-card'>
-        {listaPaises.length > 0 ? (
-          listaPaises.map((cardData) => (
-            <Card 
-              key={cardData.id}
-              nome={cardData.nome}
-              img={cardData.img} 
-              capital={cardData.capital}
-              continente={cardData.continente}
-              regiao={cardData.regiao}
-              currency={cardData.currency}
-            />
-            ))
-        ) : (
-          <p></p>
-        )}
-      </div>
+      <main>
+        <h2 id='pesquisa-h2'>Pesquise um país aleatório...</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" 
+            placeholder='nome do país em inglês'
+            value={valorInput}
+            onChange={(e) => setValorInput(e.target.value)}  
+          />
+          <button type='submit' id='btn-busca'>Buscar</button>
+        </form>
+        <div id='div-card'>
+          {listaPaises.length > 0 ? (
+            listaPaises.map((cardData) => (
+              <Card 
+                key={cardData.id}
+                nome={cardData.nome}
+                img={cardData.img} 
+                // capital={cardData.capital}
+                // continente={cardData.continente}
+                // regiao={cardData.regiao}
+                // currency={cardData.currency}
+              />
+              ))
+          ) : (
+            <p></p>
+          )}
+        {/* Segundo botão para gerar um pais aleatorio */}
+
+        </div>
+      </main>
     </>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -4,8 +4,8 @@ function Card({key, nome, img, capital, continente, regiao, currency}) {
   return (
     <div id='card'>
         <img src={img} id='flag-img'/>
-        <h2>{nome}</h2>
-        <div id='div1'>
+        <h2><a href="">{nome}</a></h2>
+        {/*<div id='div1'>
           <span>Capital:</span>
           <span><strong>{capital}</strong></span>
         </div>
@@ -25,9 +25,9 @@ function Card({key, nome, img, capital, continente, regiao, currency}) {
           <span><strong>{currency}</strong></span>
         </div>
         <div id='btn-card'>
-          <button id='edit-btn'><img src="src\images\edit.png" alt="" id='icon-edit-png'/>Editar</button>
-          <button id='del-btn'><img src="src\images\icondel.png" alt="" id='icon-del-png'/>Deletar</button>
-        </div>
+          <button id='edit-btn'><img src="public\images\edit.png" alt="" id='icon-edit-png'/>Editar</button>
+          <button id='del-btn'><img src="public\images\icondel.png" alt="" id='icon-del-png'/>Deletar</button>
+        </div> */}
     </div>
   )
 }
